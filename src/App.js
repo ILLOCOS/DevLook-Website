41652ed3ff4727add91237e8798b81c8b5c51c37
marxenion <104879438+marxenion@users.noreply.github.com>
2022-09-01 11:03:28 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import logo from './logo.svg';
+import './App.css';
+import BasicExample from './components/navbar';
+import CarouselFadeExample from './components/hero';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <>
+    <nav>
+      <BasicExample />
+    </nav>
+
+    <header>
+      <CarouselFadeExample />
+    </header>
+    
+    <main>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+    </main>
+
+    <footer>
+
+    </footer>
+
+    </>
+  );
+}
+
+export default App;
