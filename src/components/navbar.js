42bca1a6ffb8a506bed491718efe86e015fcc5bf
Aly <princealy.nueles@gmail.com>
2022-09-01 16:07:21 +0800

    customized navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,19 +1,26 @@
+import '../components/css/navbar.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 
-function CollapsibleExample() {
+function Navbarr() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar collapseOnSelect expand="lg"  variant="dark" id="nav-cont">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">PROJECT NAME</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+          <Nav className="me-auto" id="nav-list">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#careers">Find Talents</Nav.Link>
+            <Nav.Link href="#careers">Careers</Nav.Link>
+            <Nav.Link href="#aboutUs">About Us</Nav.Link>
+          
+            {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
@@ -23,18 +30,36 @@ function CollapsibleExample() {
               <NavDropdown.Item href="#action/3.4">
                 Separated link
               </NavDropdown.Item>
-            </NavDropdown>
+            </NavDropdown> */}
+
           </Nav>
+{/* 
           <Nav>
             <Nav.Link href="#deets">More deets</Nav.Link>
             <Nav.Link eventKey={2} href="#memes">
               Dank memes
             </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+          </Nav>*/}
+       
+
+        <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+           /> 
+            <Button variant="outline-secondary" className="searchbtn">Search</Button> 
+          </Form>
+
+      <Button variant="light" className='signInBtn'>Sign In</Button>
+      {/* <Button variant="light" className='joinBtn'>Join</Button> */}
+
+      
+ </Navbar.Collapse> 
       </Container>
     </Navbar>
   );
 }
 
-export default CollapsibleExample;
+export default Navbarr;
